33aba8bc1b2da882515c364d8b5ddf188a25a77c
abhijeet <abhijeet3192@gmail.com>
2015-05-08 03:44:07 +0530

    Added add-post functionality

--- task2 - The Blogging App/blog.py
+++ task2 - The Blogging App/blog.py
@@ -5,20 +5,24 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
+
 
 class Category(Base):
     __tablename__ = 'category'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
 
+
 class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
     title = Column(String(100))
     Content = Column(String(5000))
-    category_id = Column(Integer, ForeignKey('category.id'))
+    category_id = Column(Integer, ForeignKey('category.id'),
+                         nullable=True)  # When a new post will be created category-id will not be available
     category = relationship(Category)
 
 
@@ -46,27 +50,40 @@ class PostController(CementBaseController):
         description = 'This is post controller'
         arguments = [
             (['--2nd-opt'], dict(help="another option under base controller")),
+            (['extra_arguments'], dict(action='store', nargs='*')),
         ]
 
     @expose(help="Add a new post", hide=True)
     def add(self):
         print "In add post"
 
-    #       implement adding a post functionality
+        # implement adding a post functionality title and category
+        if self.app.pargs.extra_arguments:
+            title = self.app.pargs.extra_arguments[0]
+            content = self.app.pargs.extra_arguments[1]
+        engine = create_engine('sqlite:///blogapp.db')
+        Base.metadata.bind = engine
+        DBSession = sessionmaker(bind=engine)
+        session = DBSession()
+
+        new_post = Post(title=title, Content=content)
+        session.add(new_post)
+        session.commit()
+        print "Added Post"
 
     @expose(help="List all post", hide=False)
     def list(self):
         print "In list all posts"
 
-    #       implement listing of all posts
-    #       with post-id and title
+    # implement listing of all posts
+    # with post-id and title
 
     @expose(help="Search in all posts", hide=True)
     def search(self):
         print "In post Search"
 
 
-#           implement Searching a keyword in all posts
+# implement Searching a keyword in all posts
 
 class CategoryController(CementBaseController):
     class Meta:
@@ -82,7 +99,7 @@ class CategoryController(CementBaseController):
     def add(self):
         print "add a new category"
 
-    #       implement adding a category
+    # implement adding a category
 
     @expose(help="List all categories", hide=True)
     def list(self):
@@ -95,7 +112,7 @@ class CategoryController(CementBaseController):
         print "Assign a category to post"
 
 
-#       check if post has cat-id,if it has ask user if he wants to change
+# check if post has cat-id,if it has ask user if he wants to change
 #       if user confirms do it
 
 def main():
@@ -106,9 +123,10 @@ def main():
         handler.register(CategoryController)
         app.setup()
         app.run()
-#        print "HI"
+    #        print "HI"
     finally:
         app.close()
+
 
 if __name__ == '__main__':
     main()
