211783a70bce28bac90987319ba7b0c4f7952450
abhijeet <abhijeet3192@gmail.com>
2015-05-23 00:02:50 +0530

    Added support for Mysql root password setup without prompt hijack

--- task1/package_installation.py
+++ task1/package_installation.py
@@ -1,5 +1,5 @@
 __author__ = 'ads'
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 import apt
 import sys
@@ -7,26 +7,39 @@ import os
 import lsb_release
 
 release = lsb_release.get_distro_information()['CODENAME']
-os.system("echo \"deb http://ppa.launchpad.net/nginx/stable/ubuntu " + release + " main\" | sudo tee /etc/apt/sources.list.d/nginx-stable.list")
+os.system(
+    "echo \"deb http://ppa.launchpad.net/nginx/stable/ubuntu " + release + " main\" | sudo tee /etc/apt/sources.list.d/nginx-stable.list")
 os.system("apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C300EE8C")
 os.system("apt-get update")
-packages_to_install = ["mysql-server","php5-mysql","nginx","php5-fpm"]
+# Mysql Password
+while True:
+    mysql_pass = raw_input("MySql password >")
+    confirm_mysql_pass = raw_input("Confirm Mysql password >")
+    if mysql_pass==confirm_mysql_pass:
+        os.system("sudo debconf-set-selections <<< \'mysql-server mysql-server/root_password password \'" + mysql_pass)
+        os.system("sudo debconf-set-selections <<< \'mysql-server mysql-server/root_password_again password \'"+ mysql_pass)
+        break
+    else:
+        print "Sorry , Passwords do not match, try again"
+
+# List of packages you want to install
+packages_to_install = ["mysql-server", "php5-mysql", "nginx", "php5-fpm"]
 
 cache = apt.cache.Cache()
 cache.update()
 
 for package_name in packages_to_install:
-        package = cache[package_name]
-        if package.is_installed:
-                print "{package} already installed".format(package=package)
-        else:
-                package.mark_install()
-        try:
-                cache.commit()
-        except Exception, arg:
-                print >> sys.stderr, "Sorry, package installation failed [{err}]".format(err=str(arg))
+    package = cache[package_name]
+    if package.is_installed:
+        print "{package} already installed".format(package=package)
+    else:
+        package.mark_install()
+    try:
+        cache.commit()
+    except Exception, arg:
+        print >> sys.stderr, "Sorry, package installation failed [{err}]".format(err=str(arg))
 
 with open("/etc/php5/fpm/php.ini", "a") as php_config:
-        php_config.write("cgi.fix_pathinfo=0")
+    php_config.write("cgi.fix_pathinfo=0")
 
 os.system("service php5-fpm restart")
